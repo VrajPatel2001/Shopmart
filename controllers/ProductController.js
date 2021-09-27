e66ecd725333da2b368908dad46519d731002c5e
Vraj Patel <vrajnp2001@gmail.com>
2021-09-27 00:33:46 -0400

    After MVC model implemented

---
+++ controllers/ProductController.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+const productService = require("../services/ProductService");
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose =require("mongoose");
+require('dotenv').config({ path: 'config/keys.env' });
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`RESTful API is up and running on PORT ${process.env.PORT}`);
+
+    mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
+    .then(()=>{
+        console.log(`Connected to MongoDB`)
+    })
+    .catch(err=>{
+        console.log(`Error ${err}`);
+    })
+
+
+})
