e5103392b469fd10de6de95a9d6b6b2ed80def87
Vraj Patel <vrajnp2001@gmail.com>
2021-10-27 23:38:19 -0400

    Includes Cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose =require("mongoose");
+const cors = require("cors");
 
 if(process.env.NODE_ENV != "production")
 {
@@ -8,6 +9,21 @@ require('dotenv').config({ path: 'config/keys.env' });
 }
 
 const app = express();
+
+const corsOptionsDelegate = function (req, callback) 
+{
+  const allowlist = ['http://localhost:3000', 'http://127.0.0.1:3000']
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+//middleware
+app.use(cors(corsOptionsDelegate))
 
 app.use(express.json());
 const userController = require("./controllers/UserController");
